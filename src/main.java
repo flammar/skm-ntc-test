eeca3ed11b95254c6903fe5294c76612a2b632de
Michael Skidan <m.skidan@inschooltech.ru>
2025-02-06 15:46:48 +0300

    initial

---
+++ src/main.java
@@ -0,0 +1,89 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Random;
+import java.util.function.IntUnaryOperator;
+
+public class main {
+    private static String filename = "file1";
+    private static IntUnaryOperator toEven = i -> i << 1;
+    private static IntUnaryOperator toOdd = i -> (i << 1) + 1;
+
+    public static void main(String[] args) throws IOException {
+
+        // Встретив в задании слово "цифры", я понял, что речь идйт от символьном представлении числа, а не о байтовом
+        // как в java.io.DataInputStream и java.io.DataOutputStream, К тому же я понял, что числа надо разделять, чтобы
+        // "хвост" одного числа не слипся с началом другого, порождая фантомные данные.
+        // В случае бинарного формата для этого можно было бы исопльзовать тот факт, что длина байтового представления
+        // числа формата int всегда постоянна и равна 4. 
+
+        Thread odds = new Thread(newWriter(toOdd));
+        Thread evens = new Thread(newWriter(toEven));
+        Thread reader = new Thread(newReader());
+        reader.start();
+        odds.start();
+        evens.start();
+    }
+
+    private static Runnable newWriter(IntUnaryOperator intUnaryOperator) {
+        return () -> {
+            try (PrintStream ps = new PrintStream(new FileOutputStream(filename, true))) {
+                Random random = new Random();
+                while (true) {
+//                    ps.println нельзя использовать так как он делает две хзаписи в файл, числа могут слепляться
+                    ps.print(intUnaryOperator.applyAsInt(random.nextInt()) + "\n");
+                    // пауза закомментирована
+//                    sleep(200);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    private static Runnable newReader() {
+        byte[] bb = new byte[1024];
+        return () -> {
+            // Дождаться пока файл начнёт существовать, иначе Reader упадёт с ошибкой, что файл не найден.
+            while (!new File(filename).exists())
+                // промежуток времени выбран произвольно
+                sleep(100);
+            try (FileInputStream is = new FileInputStream(filename)) {
+                try {
+                    // Самое простое: всё что записано в файл, переписывать в консоль через буфер произвольного размера 
+                    while (true) {
+                        int count = is.read(bb);
+                        if (count > 0) {
+                            System.out.write(bb, 0, count);
+                        }
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } catch (IOException e1) {
+                throw new RuntimeException(e1);
+            }
+        };
+
+    }
+
+    static void sleep(long ms) {
+        // надо гарантированно проспать время, досыпая в случае прерывания
+        long ms1 = System.currentTimeMillis() + ms;
+        for (long i = ms; i > 0; i = trySleepTill(ms1));
+    }
+
+    static long trySleepTill(long ms1) {
+        try {
+            // Может, хотя и не должен по спецификации, проснуться раньше времени, надо это контролировать
+            Thread.sleep(ms1 - System.currentTimeMillis());
+            // вернуть недоспанное время
+            return ms1 - System.currentTimeMillis();
+        } catch (InterruptedException e) {
+            // Если прервали - вернуть недоспанное время
+            return ms1 - System.currentTimeMillis();
+        }
+    }
+}
